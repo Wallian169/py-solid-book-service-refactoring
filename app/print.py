0ed63eb0aab52bd75571fe8740552a08d8aa53d4
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-10-18 12:44:51 +0300

    solution

---
+++ app/print.py
@@ -0,0 +1,14 @@
+from app.models import Book
+
+
+class Printer:
+    @staticmethod
+    def print_book(book: Book, print_type: str) -> None:
+        if print_type == "console":
+            print(f"Printing the book: {book.title}...")
+            print(book.content)
+        elif print_type == "reverse":
+            print(f"Printing the book in reverse: {book.title}...")
+            print(book.content[::-1])
+        else:
+            raise ValueError(f"Unknown print type: {print_type}")

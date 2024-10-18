58948a201743f0be2969c8c7893400bd6d7d3323
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-10-18 12:56:06 +0300

    split logic of Serializer class

--- app/serializers.py
+++ app/serializers.py
@@ -3,17 +3,29 @@ import xml.etree.ElementTree as ElTree
 from app.models import Book
 
 
+class JsonSerializer:
+    @staticmethod
+    def serialize(book: Book) -> str:
+        return json.dumps({"title": book.title, "content": book.content})
+
+
+class XmlSerializer:
+    @staticmethod
+    def serialize(book: Book) -> str:
+        root = ElTree.Element("book")
+        title = ElTree.SubElement(root, "title")
+        title.text = book.title
+        content = ElTree.SubElement(root, "content")
+        content.text = book.content
+        return ElTree.tostring(root, encoding="unicode")
+
+
 class Serializer:
     @staticmethod
     def serialize(book: Book, serialize_type: str) -> str:
         if serialize_type == "json":
-            return json.dumps({"title": book.title, "content": book.content})
+            return JsonSerializer.serialize(book)
         elif serialize_type == "xml":
-            root = ElTree.Element("book")
-            title = ElTree.SubElement(root, "title")
-            title.text = book.title
-            content = ElTree.SubElement(root, "content")
-            content.text = book.content
-            return ElTree.tostring(root, encoding="unicode")
+            return XmlSerializer.serialize(book)
         else:
             raise ValueError(f"Unknown serialize type: {serialize_type}")

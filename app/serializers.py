c104bbcde1d0a5944b774e65b7cd6ef3a8bf8943
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-10-18 12:50:37 +0300

    flake8 fix

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,5 @@
 import json
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElTree
 from app.models import Book
 
 
@@ -9,11 +9,11 @@ class Serializer:
         if serialize_type == "json":
             return json.dumps({"title": book.title, "content": book.content})
         elif serialize_type == "xml":
-            root = ET.Element("book")
-            title = ET.SubElement(root, "title")
+            root = ElTree.Element("book")
+            title = ElTree.SubElement(root, "title")
             title.text = book.title
-            content = ET.SubElement(root, "content")
+            content = ElTree.SubElement(root, "content")
             content.text = book.content
-            return ET.tostring(root, encoding="unicode")
+            return ElTree.tostring(root, encoding="unicode")
         else:
             raise ValueError(f"Unknown serialize type: {serialize_type}")
